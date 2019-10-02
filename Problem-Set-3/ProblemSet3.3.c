a4ec3023bbd8efdd1c8a0225e4cbbe8ac48c9375
Anita <alin@berklee.edu>
2019-10-02 14:52:46 -0400

    Create Problemset3.3

---
+++ Problem-Set-3/ProblemSet3.3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Declare the function
+int findPrimeNumber(int,int);
+
+//Define the function so we can call it
+int findPrimeNumber(int a, int b){
+    return a + b;
+}
+
+int main(){
+    
+    return 0;
+}
+
