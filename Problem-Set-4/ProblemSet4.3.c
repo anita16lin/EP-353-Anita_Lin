841b72063f1947b846ded00d9ac54893d3d20dfd
Anita <alin@berklee.edu>
2019-10-09 21:17:23 -0400

    keep on going

---
+++ Problem-Set-4/ProblemSet4.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file;
+    file = fopen("LoremIpsum.txt", "r");
+    if(file == NULL){
+        printf("File cannot be opened or does not exist");
+        return 1;
+    }
+    
+    char string[50];
+
+    while
+    return 0;
+}
