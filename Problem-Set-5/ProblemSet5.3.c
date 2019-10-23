573848fad578b537f42959ce1389c3bc804b0a3e
Anita <alin@berklee.edu>
2019-10-23 15:09:04 -0400

    PS5.3

--- Problem-Set-5/ProblemSet5.3.c
+++ Problem-Set-5/ProblemSet5.3.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h> 
+
+int main()
+{
+    FILE *file;
+    
+    //open file
+    file = fopen("num.csv", "r");
+    
+    //check to see if file exists
+    if (file == NULL)
+    {
+        printf("File cannot be opened or does not exist.\n");
+        return 1;
+    }
+    //rewind(file);
+    if ( fseek(file, 0L, SEEK_SET) != 0 ) { 
+     /* Handle repositioning error */
+    } 
+    char string[50];
+    int commacount = 0;
+    while(fgets(string, 50, file) != NULL)
+        if (string = ','){
+            commacount ++;
+        }
+    
+
+    printf("Number of commas: %d\n", commacount);
+
+    //close open file
+    if(file) fclose(file);
+    
+    return 0;
+}
