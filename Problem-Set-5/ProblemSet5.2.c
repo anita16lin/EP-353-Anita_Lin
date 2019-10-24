b2a0db77611937fabf2fe61e16aa16aa63d7c529
Anita <alin@berklee.edu>
2019-10-23 20:33:41 -0400

    wehave the right rotations now

--- Problem-Set-5/ProblemSet5.2.c
+++ Problem-Set-5/ProblemSet5.2.c
@@ -2,12 +2,17 @@
 
 int main(){
     int s = 6;
-    int rotation = -1;
+    int rotation = 1;
     int num[6] = {1, 2, 3, 4, 5, 6};
     
+    //initialize place holder
     int c;
-    if(rotation > 0){
-        for (int j = 0; j < rotation; j++){
+
+    //the first one rotates to the left, the second one rotates to the right
+    if(rotation < 0){
+        int rotate;
+        rotate = rotation * -1;
+        for (int j = 0; j < rotate; j++){
             c = *(num + 0);
             for (int i = 1; i < 6; i ++){
                 *(num + i - 1) = *(num + i);
@@ -16,9 +21,7 @@ int main(){
         }
     }
     else {
-        int rotate;
-        rotate = rotation * -1;
-        for (int j = 0; j < rotate; j++){
+        for (int j = 0; j < rotation; j++){
             c = *(num + s - 1);
             for (int i = 0; i < s; i ++){
                 *(num + s - i) = *(num + s - i - 1);
